77f2f65116a06c1158fccbc4c85c628ce90e571a
Arturo Herrera <cripto1989@gmail.com>
2020-10-31 18:32:54 -0600

    Rename project_content folder to core

--- config/settings/base.py
+++ config/settings/base.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-APPS_DIR = BASE_DIR / "project_content"
+APPS_DIR = BASE_DIR / "core"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
